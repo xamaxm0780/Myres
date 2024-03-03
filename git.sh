96ed11ba6c478d308d1d8839874abc55a73092b1
Vasmno <xam@ham.com>
2024-03-03 17:59:16 +0300

    i want

---
+++ git.sh
@@ -0,0 +1,18 @@
+  GNU nano 6.2                         git.sh                                   
+#!/bin/bash
+touch 1.txt 
+mkdir newdir
+echo "qwerty"
+tar -cf archive.tar 1.txt
+tar -xf archive.tar 
+chmod 777 1.txt 
+chown root 1.txt 
+ps
+hostname
+cp 1.txt cr.txt
+cat 1.txt 
+ls
+mv 1.txt newdir/1.txt
+rm -rf 1.txt 
+rm -rf newdir
+cd ..
